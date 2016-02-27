278cedcb4fbb35a5cf8fd2cee95956c9a5e7b85b
Henning Post <henningpost@gmail.com>
2016-02-26 18:27:10 -0800

    common UI utilities AspectRatioImageView, BetterViewAnimator, MarginDecoration, Scrims, ViewPagerAdapter

---
+++ app/src/main/kotlin/cc/postsoft/chompy/ui/common/BetterViewAnimator.kt
@@ -0,0 +1,27 @@
+package cc.postsoft.chompy.ui.common
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ViewAnimator
+
+open class BetterViewAnimator(context: Context, attrs: AttributeSet) : ViewAnimator(context, attrs) {
+
+    var displayedChildId: Int
+        get() = getChildAt(displayedChild).id
+        set(id) {
+            if (displayedChildId == id) {
+                return
+            }
+            var i = 0
+            val count = childCount
+            while (i < count) {
+                if (getChildAt(i).id == id) {
+                    displayedChild = i
+                    return
+                }
+                i++
+            }
+            val name = resources.getResourceEntryName(id)
+            throw IllegalArgumentException("No view with ID " + name)
+        }
+}
