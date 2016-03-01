146034ac574513c54b77a7b67a11523dc54855fa
Henning Post <henningpost@gmail.com>
2016-03-01 14:40:34 -0800

    improved and extended AnimationExtensions

--- app/src/main/kotlin/cc/postsoft/chompy/extensions/AnimationExtensions.kt
+++ app/src/main/kotlin/cc/postsoft/chompy/extensions/AnimationExtensions.kt
@@ -12,87 +12,114 @@ import cc.postsoft.chompy.R
 
 
 object AnimationConstants {
-
-    const val LICKETY_SPLIT = 150L;
-
-    const val SHORT = 250L;
-
-    const val MEDIUM = 400L;
-
-    const val LONG = 600L;
+    const val LICKETY_SPLIT = 150L
+    const val SHORT = 250L
+    const val MEDIUM = 400L
+    const val LONG = 600L
 
     val TRANSLATION = dip(75)
-
     val TRANSLATION_SMALL = dip(16)
 
     val ELEVATION = dimen(R.dimen.elevation_toolbar)
 
     val ACCELERATE = AccelerateInterpolator()
-
     val DECELERATE = DecelerateInterpolator()
-
     val LINEAR_OUT_SLOW_IN = LinearOutSlowInInterpolator()
-
     val FAST_OUT_SLOW_IN = FastOutSlowInInterpolator()
-
     val FAST_OUT_LINEAR_IN = FastOutLinearInInterpolator()
-
     val ACCELERATE_DECELERATE = AccelerateDecelerateInterpolator()
-
     val OVERSHOOT = OvershootInterpolator()
-
     val CYCLE_3 = CycleInterpolator(3f)
 }
 
-fun View.slideExit() {
-    if (translationY == 0f) animate().translationY(-height.toFloat())
+fun View.fade(initAlpha: Float? = null, alpha: Float = 1f, startDelay: Long = 0,
+              build: (ViewPropertyAnimator.() -> Unit)? = null) {
+    initAlpha?.let { this.alpha = initAlpha }
+    val anim = animate()
+            .alpha(alpha)
+            .setStartDelay(startDelay)
+            .setDuration(AnimationConstants.SHORT)
+            .setInterpolator(AnimationConstants.LINEAR_OUT_SLOW_IN)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationStart(animation: Animator?) {
+                    visibility = View.VISIBLE
+                }
+
+                override fun onAnimationEnd(animation: Animator?) {
+                    if (alpha == 0f) visibility = View.INVISIBLE
+                }
+            })
+    build?.let { anim.build() }
+    anim.start()
 }
 
-fun View.slideEnter() {
-    if (translationY < 0f) animate().translationY(0f)
+fun View.translate(initX: Int? = null, initY: Int? = null, translationX: Int = 0, translationY: Int = 0,
+                   initAlpha: Float? = null, alpha: Float = 1f, startDelay: Long = 0,
+                   build: (ViewPropertyAnimator.() -> Unit)? = null) {
+    initX?.let { this.translationX = initX.toFloat() }
+    initY?.let { this.translationY = initY.toFloat() }
+    initAlpha?.let { this.alpha = initAlpha }
+    val anim = animate()
+            .alpha(alpha)
+            .translationX(translationX.toFloat())
+            .translationY(translationY.toFloat())
+            .setStartDelay(startDelay)
+            .setDuration(AnimationConstants.MEDIUM)
+            .setInterpolator(AnimationConstants.FAST_OUT_SLOW_IN)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationStart(animation: Animator?) {
+                    visibility = View.VISIBLE
+                }
+
+                override fun onAnimationEnd(animation: Animator?) {
+                    if (alpha == 0f) visibility = View.INVISIBLE
+                }
+            })
+    build?.let { anim.build() }
+    anim.start()
 }
 
-fun View.easeInVertical(vararg views: View, build: ViewPropertyAnimator.() -> Unit = { }) {
+fun easeInVertical(startDelay: Long = 0, vararg views: View, build: (ViewPropertyAnimator.() -> Unit)? = null) {
     var offset = AnimationConstants.TRANSLATION_SMALL.toFloat()
     for (view: View? in views) {
         if (view == null || view.visibility == View.VISIBLE) {
-            continue;
+            continue
         }
         view.alpha = 0.8f
         view.translationY = offset
         val anim = view.animate()
                 .alpha(1f)
                 .translationY(0f)
+                .setStartDelay(startDelay)
                 .setDuration(AnimationConstants.LONG)
                 .setInterpolator(AnimationConstants.FAST_OUT_SLOW_IN)
-                //                .withLayer() // TODO measure!
                 .setListener(object : AnimatorListenerAdapter() {
                     override fun onAnimationStart(animation: Animator) {
                         view.visibility = View.VISIBLE
                     }
                 })
-        anim.build()
+        build?.let { anim.build() }
         anim.start()
         offset *= 1.5f
     }
 }
 
-fun View.easeInVertical(build: ViewPropertyAnimator.() -> Unit = { }) {
+fun View.easeInVertical(startDelay: Long = 0, build: (ViewPropertyAnimator.() -> Unit)? = null) {
     if (visibility != View.VISIBLE) {
         alpha = 0.8f
         translationY = AnimationConstants.TRANSLATION_SMALL.toFloat()
         val anim = animate()
                 .alpha(1f)
                 .translationY(0f)
+                .setStartDelay(startDelay)
                 .setDuration(AnimationConstants.LONG)
                 .setInterpolator(AnimationConstants.FAST_OUT_SLOW_IN)
-                //                .withLayer() // TODO measure!
                 .setListener(object : AnimatorListenerAdapter() {
                     override fun onAnimationStart(animation: Animator) {
                         visibility = View.VISIBLE
                     }
                 })
-        anim.build()
+        build?.let { anim.build() }
         anim.start()
     }
 }
