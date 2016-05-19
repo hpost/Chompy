3313f5319e917df335e7c8cf95e41c29904e4694
Henning Post <henningpost@gmail.com>
2016-05-19 15:51:42 -0700

    using Kommon from jitpack

--- kommon/src/main/kotlin/cc/femto/kommon/extensions/Retrofit.kt
+++
@@ -1,4 +0,0 @@
-package cc.femto.kommon.extensions
-
-val <T> retrofit2.adapter.rxjava.Result<T>.isSuccess: Boolean
-    get() = !isError && response().isSuccessful
--- kommon/src/main/kotlin/cc/femto/kommon/extensions/TextView.kt
+++
@@ -1,7 +0,0 @@
-package cc.femto.kommon.extensions
-
-import android.widget.TextView
-
-var TextView.textColor: Int
-    get() = currentTextColor
-    set(v) = setTextColor(v)
